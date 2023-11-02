701c0571b940dcfcd28ff3289ea4051a09f6f18f
Savo Lekovic <savolekovic15@gmail.com>
2023-11-02 20:10:53 +0100

    Added necessary dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,24 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "com.dsd.kosjenka"
-    compileSdk = 33
+    compileSdk = 34
+
+    buildFeatures {
+        dataBinding = true
+        buildConfig = true
+    }
 
     defaultConfig {
         applicationId = "com.dsd.kosjenka"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -18,7 +26,11 @@ android {
     }
 
     buildTypes {
+        debug {
+            buildConfigField("String", "BASE_URL", "\"https://test.com/\"")
+        }
         release {
+            buildConfigField("String", "BASE_URL", "\"https://test.com/\"")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -26,22 +38,71 @@ android {
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "18"
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+
+    //Material Design
     implementation("com.google.android.material:material:1.10.0")
+    //Constraint Layout
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    //RecyclerView
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    //Retrofit2
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    //Gson
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    //OkHttp
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.3")
+
+    //Hilt
+    implementation("com.google.dagger:hilt-android:2.48.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.48.1")
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
+    //Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    //CardView
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+
+    //Splash Screen API
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
+    //Timber Logs
+    implementation("com.jakewharton.timber:timber:5.0.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
 }
