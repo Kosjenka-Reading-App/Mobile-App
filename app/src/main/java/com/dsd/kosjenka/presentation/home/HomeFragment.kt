c91edb301a5c29240f7a5a0c00b5c36dab4d0eaf
Savo Lekovic <savolekovic15@gmail.com>
2023-11-08 11:26:50 +0100

    Implemented data binding in ExerciseFragment.kt and HomeFragment.kt

--- app/src/main/java/com/dsd/kosjenka/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/dsd/kosjenka/presentation/home/HomeFragment.kt
@@ -1,21 +1,25 @@
 package com.dsd.kosjenka.presentation.home
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.dsd.kosjenka.R
+import com.dsd.kosjenka.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.fragment_home, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/com/dsd/kosjenka/presentation/home/exercise/ExerciseFragment.kt
+++ app/src/main/java/com/dsd/kosjenka/presentation/home/exercise/ExerciseFragment.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.dsd.kosjenka.R
+import com.dsd.kosjenka.databinding.FragmentExerciseBinding
 
 class ExerciseFragment : Fragment() {
+
+    private lateinit var binding: FragmentExerciseBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_exercise, container, false)
+    ): View {
+        binding =
+            DataBindingUtil.inflate(layoutInflater, R.layout.fragment_exercise, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
