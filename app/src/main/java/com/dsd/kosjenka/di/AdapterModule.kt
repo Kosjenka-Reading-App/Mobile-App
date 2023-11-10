2dc65df1010fdc2e2e213fc3a498e1003455498e
Faridun <faridun.mamadbekov@alumni.ucentralasia.org>
2023-11-10 15:07:52 +0100

    use data binding
    Took 4 hours 13 minutes

--- app/src/main/java/com/dsd/kosjenka/di/AdapterModule.kt
+++ app/src/main/java/com/dsd/kosjenka/di/AdapterModule.kt
@@ -3,47 +3,84 @@ package com.dsd.kosjenka.di
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.dsd.kosjenka.R
+import com.dsd.kosjenka.domain.models.Exercise
 import com.dsd.kosjenka.domain.models.UserProfile
 
 object AdapterModule {
     class UserProfilesAdapter(
-        private val userProfiles: List<UserProfile>,
-        private val profileClickListener: OnProfileClickListener
+        private val userProfiles: MutableList<UserProfile>,
+        //private val profileClickListener: OnProfileClickListener
     ) : RecyclerView.Adapter<UserProfilesAdapter.UserProfileViewHolder>() {
 
-        class UserProfileViewHolder(private val view: View, private val profileClickListener: OnProfileClickListener) : RecyclerView.ViewHolder(view), View.OnClickListener {
+        class UserProfileViewHolder(
+            private val view: View//, private val profileClickListener: OnProfileClickListener
+        ) : RecyclerView.ViewHolder(view) {
             val textView : TextView = view.findViewById(R.id.profile_list_name)
             val imageView : ImageView = view.findViewById(R.id.profile_image_view)
-            val abd = view.setOnClickListener(this)
+            //val _ls = view.setOnClickListener(this)
+            //val button : Button = view.findViewById()
+//            val profileClickListener = View.OnClickListener {
+//                profileClickListener.onProfileClick(bindingAdapterPosition)
+//            }
 
-            override fun onClick(v: View?) {
-                profileClickListener.onProfileClick(bindingAdapterPosition)
-            }
+//            override fun onClick(v: View?) {
+//                profileClickListener.onProfileClick(bindingAdapterPosition)
+//            }
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserProfileViewHolder {
-            val adapterLayout =  LayoutInflater.from(parent.context).inflate(
-                R.layout.user_profile_list_item, parent, false)
+            val adapterLayout : View = if (viewType == R.layout.user_profile_list_item)
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.user_profile_list_item, parent, false)
+            else
+                LayoutInflater.from(parent.context).inflate(
+                    R.layout.user_profile_list_item, parent, false)
 
-            return UserProfileViewHolder(adapterLayout, profileClickListener)
+            return UserProfileViewHolder(adapterLayout)
         }
 
         override fun getItemCount(): Int {
-            return userProfiles.size
+            return userProfiles.size + 1
         }
 
         override fun onBindViewHolder(holder: UserProfileViewHolder, position: Int) {
-            val userProfile = userProfiles[position]
-            holder.textView.text = userProfile.username
-            //holder.imageView.setImageResource(R.drawable.main_logo)
+            if (itemCount == position) {
+                holder.textView.text = ""
+                holder.imageView.setImageResource(R.drawable.main_logo)
+                holder.itemView.setOnClickListener { onProfileClickListener }
+
+            } else {
+                val userProfile = userProfiles[position]
+                holder.textView.text = userProfile.username
+                holder.imageView.setImageResource(R.drawable.start_image)
+                holder.itemView.setOnClickListener { onNewClickListener }
+            }
         }
 
-        interface OnProfileClickListener{
-            fun onProfileClick(position: Int)
+        override fun getItemViewType(position: Int): Int {
+            return if(userProfiles.size == position) R.layout.user_profile_list_item else R.layout.user_profile_list_item
         }
+
+//        interface OnProfileClickListener{
+//            fun onProfileClick(position: Int)
+//            fun OnAddNewClick(position: Int)
+//        }
+
+        private var onProfileClickListener: ((UserProfile) -> Unit)? = null
+        private var onNewClickListener: (() -> Unit)? = null
+
+        fun setOnProfileClickListener(listener: (UserProfile) -> Unit) {
+            onProfileClickListener = listener
+        }
+
+        fun setOnNewClickListener(listener: () -> Unit) {
+            onNewClickListener = listener
+        }
+
     }
 }
--- app/src/main/java/com/dsd/kosjenka/presentation/user_profiles/UserProfilesFragment.kt
+++ app/src/main/java/com/dsd/kosjenka/presentation/user_profiles/UserProfilesFragment.kt
@@ -4,46 +4,64 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.dsd.kosjenka.R
+import com.dsd.kosjenka.databinding.FragmentHomeBinding
+import com.dsd.kosjenka.databinding.FragmentUserProfilesBinding
 import com.dsd.kosjenka.di.AdapterModule
 import com.dsd.kosjenka.domain.models.UserProfile
+import com.dsd.kosjenka.presentation.home.ExerciseAdapter
 
-class UserProfilesFragment : Fragment(), AdapterModule.UserProfilesAdapter.OnProfileClickListener {
+class UserProfilesFragment : Fragment(){
 
-    private val profilesList = listOf<UserProfile>(
-        UserProfile("user1", 5.0),
-        UserProfile("user2", 1.0),
-        UserProfile("user3", 2.0))
+    private lateinit var profilesList : MutableList<UserProfile>
+    private lateinit var userProfilesAdapter: AdapterModule.UserProfilesAdapter
+    private lateinit var binding: FragmentUserProfilesBinding
+    private val viewModel by lazy { ViewModelProvider(this)[UserProfilesViewModel::class.java] }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        return inflater.inflate(R.layout.fragment_user_profiles, container, false)
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_user_profiles, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-<<<<<<< HEAD
-=======
-        val profilesList = listOf<UserProfile>(
-            UserProfile("user1", 5.0),
-            UserProfile("user1", 5.0),
-            UserProfile("user1", 5.0)
-        )
->>>>>>> 6aaff30560c360f17bdad804642be1c9f62d93a5
-
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view_user_profiles)
-        recyclerView.adapter = AdapterModule.UserProfilesAdapter(profilesList, this)
-        recyclerView.setHasFixedSize(true)
-
+        setupRecycler()
+        getProfiles()
     }
 
-    override fun onProfileClick(position: Int) {
-        findNavController().navigate(R.id.action_userProfilesFragment_to_homeFragment)
+    private fun setupRecycler(){
+        userProfilesAdapter = AdapterModule.UserProfilesAdapter(profilesList)
+        userProfilesAdapter.setOnProfileClickListener {
+            findNavController() //navigate
+        }
+        userProfilesAdapter.setOnNewClickListener {
+            findNavController() //navigate
+        }
+
+        binding.recyclerViewUserProfiles.adapter = userProfilesAdapter
+        binding.recyclerViewUserProfiles.setHasFixedSize(true)
     }
 
+    private fun getProfiles(){
+        profilesList = mutableListOf<UserProfile>(
+            UserProfile("user1", 5.0),
+            UserProfile("user2", 1.0),
+            UserProfile("user3", 2.0))
+    }
+
+//    override fun onProfileClick(position: Int) {
+//        findNavController().navigate(R.id.action_userProfilesFragment_to_homeFragment)
+//    }
+//
+//    override fun OnAddNewClick(position: Int) {
+//        TODO("Not yet implemented")
+//    }
 }
