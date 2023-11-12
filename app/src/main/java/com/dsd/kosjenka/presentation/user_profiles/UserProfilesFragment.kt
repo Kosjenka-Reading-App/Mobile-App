883c99f01a225e279a2030ebe4bf7b77a3899cbe
Savo Lekovic <savolekovic15@gmail.com>
2023-11-12 20:40:32 +0100

    cleanup

--- app/src/main/java/com/dsd/kosjenka/presentation/user_profiles/UserProfilesFragment.kt
+++ app/src/main/java/com/dsd/kosjenka/presentation/user_profiles/UserProfilesFragment.kt
@@ -1,36 +1,27 @@
 package com.dsd.kosjenka.presentation.user_profiles
 
-import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.text.TextUtils
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.OnClickListener
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.RecyclerView
 import com.dsd.kosjenka.R
 import com.dsd.kosjenka.databinding.AlertAddProfileBinding
-import com.dsd.kosjenka.databinding.FragmentHomeBinding
 import com.dsd.kosjenka.databinding.FragmentUserProfilesBinding
 import com.dsd.kosjenka.di.AdapterModule
 import com.dsd.kosjenka.domain.models.UserProfile
-import com.dsd.kosjenka.presentation.auth.login.LoginFragmentDirections
-import com.dsd.kosjenka.presentation.home.ExerciseAdapter
-import timber.log.Timber
-import java.util.logging.Logger
 
 class UserProfilesFragment : Fragment(),
     AdapterModule.UserProfilesAdapter.ProfileItemClickListener {
 
-    private lateinit var profilesList : MutableList<UserProfile>
+    private lateinit var profilesList: MutableList<UserProfile>
     private lateinit var userProfilesAdapter: AdapterModule.UserProfilesAdapter
     private lateinit var binding: FragmentUserProfilesBinding
     private val viewModel by lazy { ViewModelProvider(this)[UserProfilesViewModel::class.java] }
@@ -39,7 +30,9 @@ class UserProfilesFragment : Fragment(),
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
-        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_user_profiles, container, false)
+        binding = DataBindingUtil.inflate(
+            layoutInflater, R.layout.fragment_user_profiles, container, false
+        )
         return binding.root
     }
 
@@ -49,7 +42,7 @@ class UserProfilesFragment : Fragment(),
         getProfiles()
     }
 
-    private fun setupRecycler(){
+    private fun setupRecycler() {
         userProfilesAdapter = AdapterModule.UserProfilesAdapter(this)
 //        userProfilesAdapter.setOnProfileClickListener {
 //            findNavController().navigate(UserProfilesFragmentDirections
@@ -64,48 +57,49 @@ class UserProfilesFragment : Fragment(),
         binding.recyclerViewUserProfiles.setHasFixedSize(true)
     }
 
-    private fun getProfiles(){
+    private fun getProfiles() {
         profilesList = mutableListOf(
-            UserProfile(0, "user1", 5.0),
-            UserProfile(1, "user2", 1.0),
-            UserProfile(2, "user3", 2.0))
+            UserProfile(0, "user1", 5.0), UserProfile(1, "user2", 1.0), UserProfile(2, "user3", 2.0)
+        )
 
         userProfilesAdapter.differ.submitList(profilesList)
     }
 
     override fun onProfileClick(profile: UserProfile) {
         Toast.makeText(context, profile.username, Toast.LENGTH_SHORT).show()
-        findNavController().navigate(UserProfilesFragmentDirections
-            .actionUserProfilesFragmentToHomeFragment(profile.profileId))
+        findNavController().navigate(
+            UserProfilesFragmentDirections.actionUserProfilesFragmentToHomeFragment(profile.profileId)
+        )
     }
 
     override fun onAddProfileClick() {
         addProfileDialog()
     }
 
-    private fun addProfileDialog(){
+    private fun addProfileDialog() {
         val builder = AlertDialog.Builder(this.context)
 
-        val dialogBinding : AlertAddProfileBinding = DataBindingUtil.inflate(layoutInflater,
-            R.layout.alert_add_profile, null, false)
+        val dialogBinding: AlertAddProfileBinding = DataBindingUtil.inflate(
+            layoutInflater, R.layout.alert_add_profile, null, false
+        )
 
-        with(builder){
+        with(builder) {
             setTitle("Add Profile")
             setMessage("Please enter the new user-name")
             setView(dialogBinding.root)
-            setPositiveButton("Add") {dialog: DialogInterface?, which: Int ->
+            setPositiveButton("Add") { dialog: DialogInterface?, which: Int ->
                 //Toast.makeText(context, "New profile Add", Toast.LENGTH_SHORT).show()
-                if (isValidUsername(dialogBinding.addProfileEditText.text.toString())){
+                if (isValidUsername(dialogBinding.addProfileEditText.text.toString())) {
                     createNewProfile(dialogBinding.addProfileEditText.text.toString())
                 }
             }
-            setNegativeButton("Cancel") {dialog: DialogInterface?, _: Int -> dialog?.cancel()}
+            setNegativeButton("Cancel") { dialog: DialogInterface?, _: Int -> dialog?.cancel() }
             show()
         }
 
     }
 
-    private fun createNewProfile(username: String){
+    private fun createNewProfile(username: String) {
         val newuser = UserProfile(profilesList.size, username, 0.0)
         profilesList.add(newuser)
         //Timber.tag("UserProfiles").d(profilesList.toString())
@@ -113,7 +107,7 @@ class UserProfilesFragment : Fragment(),
         userProfilesAdapter.notifyDataSetChanged()
     }
 
-    private fun isValidUsername(username: String) : Boolean{
+    private fun isValidUsername(username: String): Boolean {
         return !TextUtils.isEmpty(username)
     }
 }
