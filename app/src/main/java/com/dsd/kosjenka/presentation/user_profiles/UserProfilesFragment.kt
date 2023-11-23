926630efec962ebc1e14171bc63cdb811fa598ef
Faridun <faridun.mamadbekov@alumni.ucentralasia.org>
2023-11-23 23:49:50 +0100

    Revert "fixed userprofile recycler view layout"
    This reverts commit 801d99016f8c79b46773d37c20294b6a54304f98.

--- app/src/main/java/com/dsd/kosjenka/presentation/user_profiles/UserProfilesFragment.kt
+++ app/src/main/java/com/dsd/kosjenka/presentation/user_profiles/UserProfilesFragment.kt
@@ -18,7 +18,6 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.GridLayoutManager
 import com.dsd.kosjenka.R
 import com.dsd.kosjenka.databinding.AlertAddProfileBinding
 import com.dsd.kosjenka.databinding.FragmentUserProfilesBinding
@@ -64,11 +63,8 @@ class UserProfilesFragment : Fragment(),
     private fun setupRecycler(){
         userProfilesAdapter = AdapterModule.UserProfilesAdapter(this)
 
-        binding.recyclerViewUserProfiles.apply {
-            setHasFixedSize(true)
-            layoutManager = GridLayoutManager(this.context, 2)
-            adapter = userProfilesAdapter
-        }
+        binding.recyclerViewUserProfiles.adapter = userProfilesAdapter
+        binding.recyclerViewUserProfiles.setHasFixedSize(true)
     }
 
 //    private fun getProfiles(){
